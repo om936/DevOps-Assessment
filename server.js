777dc136914bbd0068429fc8dba1443a3607361e
omkar <omkarmule16@gmail.com>
2024-01-05 15:16:09 +0530

    msg

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world omkar here devops');
+  res.send('Hello world omkar here devops pune');
 });
 
 app.listen(PORT, HOST, () => {
